251648781787a1265025b55464b9c68a9f1e0226
nnhathoai <nnhathoai@gmail.com>
2019-07-02 16:53:42 +0700

    '56%'

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -12,7 +12,7 @@
                             d="M1344 800v64q0 14-9 23t-23 9h-352v352q0 14-9 23t-23 9h-64q-14 0-23-9t-9-23v-352h-352q-14 0-23-9t-9-23v-64q0-14 9-23t23-9h352v-352q0-14 9-23t23-9h64q14 0 23 9t9 23v352h352q14 0 23 9t9 23zm128 448v-832q0-66-47-113t-113-47h-832q-66 0-113 47t-47 113v832q0 66 47 113t113 47h832q66 0 113-47t47-113zm128-832v832q0 119-84.5 203.5t-203.5 84.5h-832q-119 0-203.5-84.5t-84.5-203.5v-832q0-119 84.5-203.5t203.5-84.5h832q119 0 203.5 84.5t84.5 203.5z" />
                         </svg>
                 </div>
-                <li v-for="key in datas" :key="key">{{key}}</li>
+                <li @click="showRoomChat" v-for="row in datas" v-bind:value = "row.key">{{row.name}}</li>
             </ul>
         </div>
         <div class="box-center">
@@ -119,11 +119,15 @@ import { debuglog } from 'util';
                     timer: 2000
                 })
                 .then(
-                    (result) => {_this.$router.push('/chat')}
+                    (result) => {_this.$router.go()}
                 )
             },
+            showRoomChat(){
+                console.log("Mai làm")
+            }
         },
-        created: function() {
+        created : function() {
+
             var _this = this;
             var rootRef = fb.database().ref('rooms');
             rootRef.once("value", function(snapshot) {
@@ -131,9 +135,10 @@ import { debuglog } from 'util';
                 const keys = Object.keys(datas);
                 for (const key of keys) {
                     var ob = {
-                        key: snapshot.val().key
+                        name: datas[key].name,
+                        key: key
                     }
-                    _this.datas[key] = datas[key];
+                    _this.datas.push(ob);
                 }
             });
         }
