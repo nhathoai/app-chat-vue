10bf053c1bfc6df11ea0e38e7d6a4abc4a949764
nnhathoai <nnhathoai@gmail.com>
2019-07-04 17:01:38 +0700

    4/7/2019

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -12,16 +12,16 @@
                             d="M1344 800v64q0 14-9 23t-23 9h-352v352q0 14-9 23t-23 9h-64q-14 0-23-9t-9-23v-352h-352q-14 0-23-9t-9-23v-64q0-14 9-23t23-9h352v-352q0-14 9-23t23-9h64q14 0 23 9t9 23v352h352q14 0 23 9t9 23zm128 448v-832q0-66-47-113t-113-47h-832q-66 0-113 47t-47 113v832q0 66 47 113t113 47h832q66 0 113-47t47-113zm128-832v832q0 119-84.5 203.5t-203.5 84.5h-832q-119 0-203.5-84.5t-84.5-203.5v-832q0-119 84.5-203.5t203.5-84.5h832q119 0 203.5 84.5t84.5 203.5z" />
                         </svg>
                 </div>
-                <li @click="showRoomChat" v-for="row in datas" v-bind:value = "row.key">{{row.name}}</li>
+                <li @click="showRoomChat" v-for="row in datas" v-bind:data-value = "row.key">{{row.name}}</li>
             </ul>
         </div>
         <div class="box-center">
             <div class="display-content">
                 <h2>trò chuyện bốn phương</h2>
                 <hr>
+                <div v-html = "html"></div>
             </div>
             <div contenteditable="true" class="box-chat">
-                dafads
             </div>
         </div>
         <transition name="fade">
@@ -73,7 +73,9 @@ import { debuglog } from 'util';
                 errEmpty: false,
                 errBtn: true,
                 nameRoom: '',
-                datas: []
+                datas: [],
+                idRoom: '',
+                html: ''
             };
         },
         methods: {
@@ -122,8 +124,16 @@ import { debuglog } from 'util';
                     (result) => {_this.$router.go()}
                 )
             },
-            showRoomChat(){
-                console.log("Mai làm")
+            showRoomChat(e, binding){
+               
+                var _this = this;
+                var el = e.target;
+                this.idRoom = el.dataset.value;
+                var rootRef = fb.database().ref('rooms/' + this.idRoom + '/messages');
+                rootRef.once("value", function(snapshot) {
+                    const datas = snapshot.val();
+                    _this.html = `<div><span>${datas.obj.name}</span><a>${datas.obj.time}</a><span>${datas.obj.mess}</span></div>`;
+                })
             }
         },
         created : function() {
